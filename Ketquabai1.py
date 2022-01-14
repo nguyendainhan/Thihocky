ec2597db3644cbf3ee41680643b6694394097b21
nguyendainhan <90501468+nguyendainhan@users.noreply.github.com>
2022-01-14 09:47:13 +0700

    Delete Ketquabai1.py

--- Ketquabai1.py
+++
@@ -1,8 +0,0 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
- RESTART: C:\Thư Viện Sách\Xác suất thống kê\Nguyễn Đại Nhân CNTT14-05\Bai1.py 
-Xin chào, tôi là: Nhan
-Nhập vào một số:4
-{1: 1, 2: 4, 3: 9, 4: 16}
->>> 
